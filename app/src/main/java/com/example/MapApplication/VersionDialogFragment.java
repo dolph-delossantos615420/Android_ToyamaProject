408079d1c4ce7d8c1a7f8d650e0460192c0e6e8d
WhitePawnJN <rose.freelance.89.jp@gmail.com>
2015-11-10 14:09:22 +0900

    _modified_
    アプリのバージョン表示に関して修正しました。
    VersionDialogFragmentでアプリのバージョンの取得と表示をコーディングしました。
    build.gradleでアプリのバージョンの設定を修正しました。

--- app/src/main/java/com/example/MapApplication/VersionDialogFragment.java
+++ app/src/main/java/com/example/MapApplication/VersionDialogFragment.java
@@ -5,8 +5,11 @@ package com.example.MapApplication;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v4.app.DialogFragment;
 
 public class VersionDialogFragment extends DialogFragment
@@ -15,9 +18,20 @@ implements OnClickListener{
 	@Override
 	public Dialog onCreateDialog(android.os.Bundle savedInstanceState)
 	{
+		Context context = getContext();
+		PackageManager pm = context.getPackageManager();
+		String message = "Version 0";
+		try {
+			PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+			message = pi.versionName;
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		// TODO バージョン情報を表示する
 		return new AlertDialog.Builder(getActivity())
 		    .setTitle(R.string.action_version)
-		    .setMessage(R.string.versionMsg)
+		    .setMessage(message)
 		    .setPositiveButton("OK", this)
 		    .create();
 	}
